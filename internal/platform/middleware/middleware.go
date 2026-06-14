2fa253277464e8a79fa403a5d08655486f367942
Riyan Amanda <ryant.n92@gmail.com>
2026-06-14 15:19:38 +0700

    fix: add panic recover

--- internal/platform/middleware/middleware.go
+++ internal/platform/middleware/middleware.go
@@ -2,12 +2,13 @@ package middleware
 
 import (
 	"github.com/labstack/echo/v5"
+	echomw "github.com/labstack/echo/v5/middleware"
 
 	"github.com/riyanamanda/helpdesk-backend/internal/platform/config"
 )
 
 func Register(e *echo.Echo, cfg config.App) {
+	e.Use(echomw.Recover())
 	e.Use(corsMiddleware(cfg.CORSOrigins))
-
 	e.Use(requestID())
 }
